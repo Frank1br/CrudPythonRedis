05c1d0a941e7efaaf1615598b41642831f215d3b
Frank Oliveira <fm0325836@gmail.com>
2025-10-20 15:45:36 -0300

    feat: Initialize Django app structure with models, views, and admin configuration

---
+++ app/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Tarefas(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField()
+    dataCriacao = models.DateTimeField(auto_now_add=True)
+    concluida = models.BooleanField(default=False)
+
+    #Enum 
+    class StatusTarefa(models.TextChoices):
+        PENDENTE = "Pendente", "Pendente"
+        EM_PROGRESSO = "Em Progresso", "Em Progresso"
+        CONCLUIDA = "Concluida", "Concluida"
+
+    
